40909dadf158945b5414c64513593e631a7c1672
stephanmg <stephan@syntaktischer-zucker.de>
2022-03-20 16:54:08 +0100

    Setup express app.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,15 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const morgan = require("morgan")
+const res = require("express/lib/response")
 
 const app = express()
 app.use(morgan("combined"))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT || 8181, "localhost")
+
+app.get('/status', (req, res) => {
+    res.send({ message: 'hello world!'})
+})
