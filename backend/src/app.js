5bb063473c00746f5e764a299d22b567f5edf100
stephanmg <stephan@syntaktischer-zucker.de>
2022-03-20 16:45:57 +0100

    Fix eslint config.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,7 @@
-console.log("hello")
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const morgan = require("morgan")
+
+const app = express()
+app.use(morgan("combined"))
