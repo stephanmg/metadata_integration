1edaaa0370cb636756b2ace7159035b746225d73
stephanmg <stephan@syntaktischer-zucker.de>
2022-03-21 17:30:52 +0100

    Adding utility module.

---
+++ backend/src/api.js
@@ -0,0 +1,20 @@
+const get_number_of_files_by_projects = () =>
+{
+	return 1000
+}
+
+const get_disk_usage_by_projects = () =>
+{
+	return -100
+}
+
+const get_number_of_projects = () =>
+{
+	return 23
+}
+
+module.exports = {
+	get_number_of_files_by_projects,
+	get_disk_usage_by_projects,
+	get_number_of_projects
+}
