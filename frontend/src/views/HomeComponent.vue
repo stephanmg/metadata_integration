1ef91ba74393960674dc6e145b851a148e2d854c
stephanmg <stephan@syntaktischer-zucker.de>
2022-04-01 18:34:14 +0200

    Adding HomeComponent.

---
+++ frontend/src/views/HomeComponent.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <!-- Main web application -->
+    <ul>
+      <li>
+        <a
+          href="/register"
+        >
+          Register
+        </a>
+       <li>
+        <a
+          href="/login"
+        >
+          Login
+        </a>
+       <li>
+        <a
+          href="/query"
+        >
+          Query builder
+        </a>
+      </li>
+    </ul>
+    <br/>
+    <br/>
+    <br/>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data () {
+    return {
+      msg: 'Welcome to metadata database access'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
