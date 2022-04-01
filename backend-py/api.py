06529e6c9160653fc26479680c91ccb7db0f84ea
stephanmg <stephan@syntaktischer-zucker.de>
2022-04-01 18:38:09 +0200

    Adding Python backend method stubs.

--- backend-py/api.py
+++ backend-py/api.py
@@ -1,8 +1,19 @@
 def get_number_of_files_by_projects():
+	""" Get number of files by project """
 	return 1000
 
 def get_disk_usage_by_projects():
+	""" Get disk usage by projects """
 	return -100
 
 def get_number_of_projects():
+	""" Get number of projects """
 	return 23
+
+def get_graph_view():
+	""" Get graph view of query """
+	pass
+
+def get_pie_chart_view():
+	""" Get pie chart view """ 
+	pass
