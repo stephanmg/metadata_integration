cbcbd33934953255157d704e8a05c86e3198b8d8
stephanmg <stephan@syntaktischer-zucker.de>
2022-04-01 18:39:38 +0200

    Adding Javascript backend method stubs.

--- backend/src/api.js
+++ backend/src/api.js
@@ -13,8 +13,20 @@ const get_number_of_projects = () =>
 	return 23
 }
 
+const get_graph_view  = () =>
+{
+	return null
+}
+
+const get_pie_chart_view = () =>
+{
+	return null
+}
+
 module.exports = {
 	get_number_of_files_by_projects,
 	get_disk_usage_by_projects,
-	get_number_of_projects
+	get_number_of_projects,
+	get_graph_view,
+	get_pie_chart_view
 }
